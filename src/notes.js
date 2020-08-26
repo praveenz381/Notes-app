291cef119bd6190da923c08571f906f9a34eb816
Praveen Kumar K <praveenz381.kk@gmail.com>
2020-08-26 22:54:06 +0530

    added folders

---
+++ src/notes.js
@@ -0,0 +1,14 @@
+// for testing purpose 
+
+// const notes = [{
+//     key:1,
+//     title:"batman",
+//     content:"iam batman"
+// },
+// {
+//     key:1,
+//     title:"thor",
+//     content:"lord of thunder"
+// }]
+
+// export default notes
